5f0cdbd25a1166dda6cdf9127df9faeb1d8db9eb
dumont <164788903+whft@users.noreply.github.com>
2025-04-26 08:08:40 -0400

    Update loader.lua

---
+++ loader.lua
@@ -0,0 +1,3 @@
+getgenv()._ = "dumont loves u all | If you pay for this script you got scammed, this script is completely free ok"
+
+loadstring(game:HttpGet("https://github.com/whft/rost/blob/main/main.lua"))()
